d187cfa076e46fec47f318fd1750780037e0b1f6
Kyle Blacker <coachblacker@gmail.com>
2023-03-01 10:21:46 -0700

    Feat: Framework for project set up.

---
+++ logger.py
@@ -0,0 +1,12 @@
+class Logger:
+    pass
+
+# a is append w is overwrite and r is readme
+# file = open("log.txt", "r")
+
+# What information goes into the message
+# This will only append it to the end of the line so need to use \n
+# file.write("")
+
+# Wrtie the message and then close it
+# file.close()
